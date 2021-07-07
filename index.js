6299bf313af98b131e12741e8c7f32f166b161b0
William Crownover <williamcrownover1@gmail.com>
2021-07-06 19:34:14 -0700

    Added initial development files to repo and required specific packages for program to run

---
+++ index.js
@@ -0,0 +1,22 @@
+// TODO: Include packages needed for this application
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title"
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
