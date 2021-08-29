56a5a16111ac4815a1873e26ba137230ad201e41
William Crownover <williamcrownover1@gmail.com>
2021-08-28 19:15:48 -0700

    Formatting using eslint

--- index.js
+++ index.js
@@ -1,71 +1,71 @@
 // Complete: Include packages needed for this application
-const generateMarkdown = require("./utils/generateMarkdown");
-const fs = require("fs");
-const inquirer = require("inquirer");
+const generateMarkdown = require( './utils/generateMarkdown' );
+const fs = require( 'fs' );
+const inquirer = require( 'inquirer' );
 
 // Complete: Create an array of questions for user input
 const questions = [
-    {
-        type: "input",
-        message: "What is the title of your project?",
-        name: "title"
-    },
-    {
-        type: "input",
-        message: "Describe your project.",
-        name: "description"
-    },
-    {
-        type: "input",
-        message: "How do you install the program?",
-        name: "installation"
-    },
-    {
-        type: "input",
-        message: "How do you use this application?",
-        name: "usage"
-    },
-    {
-        type: "input",
-        message: "What are the guidelines for contributing?",
-        name: "contribute"
-    },
-    {
-        type: "input",
-        message: "What are the tests for this program?",
-        name: "tests"
-    },
-    {
-        type: "list",
-        message: "Choose a license for your project.",
-        name: "license",
-        choices: ["No License","Apache License 2.0","GNU General Public License v3.0","MIT License"]
-    },
-    {
-        type: "input",
-        message: "What is your GitHub username?",
-        name: "username",
-    },
-    {
-        type: "input",
-        message: "What email should user's questions be sent to?",
-        name: "email",
-    }
+	{
+		type: 'input',
+		message: 'What is the title of your project?',
+		name: 'title'
+	},
+	{
+		type: 'input',
+		message: 'Describe your project.',
+		name: 'description'
+	},
+	{
+		type: 'input',
+		message: 'How do you install the program?',
+		name: 'installation'
+	},
+	{
+		type: 'input',
+		message: 'How do you use this application?',
+		name: 'usage'
+	},
+	{
+		type: 'input',
+		message: 'What are the guidelines for contributing?',
+		name: 'contribute'
+	},
+	{
+		type: 'input',
+		message: 'What are the tests for this program?',
+		name: 'tests'
+	},
+	{
+		type: 'list',
+		message: 'Choose a license for your project.',
+		name: 'license',
+		choices: ['No License', 'Apache License 2.0', 'GNU General Public License v3.0', 'MIT License']
+	},
+	{
+		type: 'input',
+		message: 'What is your GitHub username?',
+		name: 'username',
+	},
+	{
+		type: 'input',
+		message: 'What email should user\'s questions be sent to?',
+		name: 'email',
+	}
 ];
 
 // Complete: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(`./output/${fileName}`, data, (err) => err ? console.error(err) : console.log('Success!'));
+function writeToFile( fileName, data ) {
+	fs.writeFile( `./output/${fileName}`, data, ( err ) => err ? console.error( err ) : console.log( 'Success!' ) );
 }
 
 // Complete: Create a function to initialize app
 function init() {
-    inquirer
-        .prompt(questions)
-        .then((response) => {
-            const markdownString = generateMarkdown(response);
-            writeToFile("README.md", markdownString);
-        });
+	inquirer
+		.prompt( questions )
+		.then( ( response ) => {
+			const markdownString = generateMarkdown( response );
+			writeToFile( 'README.md', markdownString );
+		} );
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,125 +1,125 @@
-//FUNCTIONS
-function renderTitle(title) {
-  return `# ${title} \n`;
+// FUNCTIONS
+function renderTitle( title ) {
+	return `# ${title} \n`;
 }
 
-function renderDescription(description) {
-  return `## Description \n`+
+function renderDescription( description ) {
+	return '## Description \n'+
     `${description} \n`;
 }
 
-function renderTableOfContents(license) {
-  let licenseTemplate;
+function renderTableOfContents( license ) {
+	let licenseTemplate;
 
-  // If there was no license selected, the section is excluded from the tabel of contents
-  switch (license) {
-    case "No License":
-      licenseTemplate = "";
-      break;
-    default:
-      licenseTemplate = `* [License](#license) \n`;
-  }
+	// If there was no license selected, the section is excluded from the tabel of contents
+	switch ( license ) {
+	case 'No License':
+		licenseTemplate = '';
+		break;
+	default:
+		licenseTemplate = '* [License](#license) \n';
+	}
 
-  return `## Table of Contents \n`+
-    `* [Installation](#installation) \n`+
-    `* [Usage](#usage) \n`+
+	return '## Table of Contents \n'+
+    '* [Installation](#installation) \n'+
+    '* [Usage](#usage) \n'+
     licenseTemplate +
-    `* [Contributing](#contributing) \n`+
-    `* [Tests](#tests) \n`+
-    `* [Questions](#questions) \n`;
+    '* [Contributing](#contributing) \n'+
+    '* [Tests](#tests) \n'+
+    '* [Questions](#questions) \n';
 }
 
-function renderInstallation(install) {
-  return `## Installation \n`+
+function renderInstallation( install ) {
+	return '## Installation \n'+
     `${install} \n`;
 }
 
-function renderUsage(usage) {
-  return `## Usage \n`+
+function renderUsage( usage ) {
+	return '## Usage \n'+
     `${usage} \n`;
 }
 
 // Complete: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  switch(license) {
-    case "Apache License 2.0":
-      return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]";
-    case "GNU General Public License v3.0":
-      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]";
-    case "MIT License":
-      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]";
-    default:
-      return "";
-  }
+function renderLicenseBadge( license ) {
+	switch( license ) {
+	case 'Apache License 2.0':
+		return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]';
+	case 'GNU General Public License v3.0':
+		return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]';
+	case 'MIT License':
+		return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]';
+	default:
+		return '';
+	}
 }
 
 // Complete: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
-  switch(license) {
-    case "Apache License 2.0":
-      return "(https://opensource.org/licenses/Apache-2.0)";
-    case "GNU General Public License v3.0":
-      return "(https://www.gnu.org/licenses/gpl-3.0)";
-    case "MIT License":
-      return "(https://opensource.org/licenses/MIT)";
-    default:
-      return "";
-  }
+function renderLicenseLink( license ) {
+	switch( license ) {
+	case 'Apache License 2.0':
+		return '(https://opensource.org/licenses/Apache-2.0)';
+	case 'GNU General Public License v3.0':
+		return '(https://www.gnu.org/licenses/gpl-3.0)';
+	case 'MIT License':
+		return '(https://opensource.org/licenses/MIT)';
+	default:
+		return '';
+	}
 }
 
 // Renders the full license badge with working link
-function renderLinkedLicenseBadge(license) {
-  switch(license) {
-    case "No License":
-      return "";
-    default:
-      return `${renderLicenseBadge(license)}${renderLicenseLink(license)} \n\n`;
-  }
+function renderLinkedLicenseBadge( license ) {
+	switch( license ) {
+	case 'No License':
+		return '';
+	default:
+		return `${renderLicenseBadge( license )}${renderLicenseLink( license )} \n\n`;
+	}
 }
 
 // Complete: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {
-  switch(license) {
-    case "No License":
-      return "";
-    default:
-      return `## License \n`+
-        `This project is covered under the following license. Click the badge for more information. \n\n`+
-        `${renderLinkedLicenseBadge(license)}`;
-  }
+function renderLicenseSection( license ) {
+	switch( license ) {
+	case 'No License':
+		return '';
+	default:
+		return '## License \n'+
+        'This project is covered under the following license. Click the badge for more information. \n\n'+
+        `${renderLinkedLicenseBadge( license )}`;
+	}
 }
 
-function renderContributing(contribute) {
-  return `## Contributing \n`+
+function renderContributing( contribute ) {
+	return '## Contributing \n'+
     `${contribute} \n`;
 }
 
-function renderTests(tests) {
-  return `## Tests \n`+
+function renderTests( tests ) {
+	return '## Tests \n'+
     `${tests} \n`;
 }
 
-function renderQuestions(username, email) {
-  return `## Questions \n`+
+function renderQuestions( username, email ) {
+	return '## Questions \n'+
     `${username} [GitHub Profile](https://github.com/${username}) \n\n`+
     `If you have questions please email me at ${email} \n`;
 }
 
 // Complete: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `${renderTitle(data.title)} \n`+
-    `${renderLinkedLicenseBadge(data.license)}`+
-    `${renderDescription(data.description)} \n`+
-    `${renderTableOfContents(data.license)} \n`+
-    `${renderInstallation(data.installation)} \n`+
-    `${renderUsage(data.usage)} \n`+
-    `${renderLicenseSection(data.license)}`+
-    `${renderContributing(data.contribute)} \n`+
-    `${renderTests(data.tests)} \n`+
-    `${renderQuestions(data.username, data.email)}`;
+function generateMarkdown( data ) {
+	return `${renderTitle( data.title )} \n`+
+    `${renderLinkedLicenseBadge( data.license )}`+
+    `${renderDescription( data.description )} \n`+
+    `${renderTableOfContents( data.license )} \n`+
+    `${renderInstallation( data.installation )} \n`+
+    `${renderUsage( data.usage )} \n`+
+    `${renderLicenseSection( data.license )}`+
+    `${renderContributing( data.contribute )} \n`+
+    `${renderTests( data.tests )} \n`+
+    `${renderQuestions( data.username, data.email )}`;
 }
 
 module.exports = generateMarkdown;
